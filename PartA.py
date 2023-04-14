87323960df41235da19ce5e8708d3679976af86b
Jessica <allenje1@uci.edu>
2023-04-13 21:51:42 -0700

    Added outline of functions

---
+++ PartA.py
@@ -0,0 +1,21 @@
+# Tokenize function #
+def tokenize(TextFilePath):
+    # list will hold tokens and be returned later
+    tokenList = []
+
+
+    return tokenList
+
+
+# computeWordFrequencies #
+def computeWordFrequencies(TokenList):
+    # maps tokens and their count. Will be returned later.
+    tokenCountMap = {}
+
+    return tokenCountMap
+
+
+# print #
+def printFrequencies(TokenCountMap) -> None:
+    # sort dict
+    
---
+++ PartB.py
@@ -0,0 +1,7 @@
+# intersection #
+def intersection(TextFile1, TextFile2):
+    # number of common tokens
+    intWords = 0
+
+
+    return intWords
