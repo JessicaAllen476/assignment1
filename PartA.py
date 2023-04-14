a9be77a2014cf8cd90cc9a85d615f519d2f8473e
Jessica <allenje1@uci.edu>
2023-04-13 22:05:15 -0700

    Flushes out tokenize function

--- PartA.py
+++ PartA.py
@@ -1,8 +1,32 @@
+import re
+
 # Tokenize function #
 def tokenize(TextFilePath):
     # list will hold tokens and be returned later
     tokenList = []
 
+    # open & read text file
+    f = open(TextFilePath, 'r')
+
+    # reading file line by line
+    currentLine = f.readline()
+    while currentLine:
+        # find all words (including words with an apostrophe)
+        # credit for regex guide: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+        words = re.findall(r'[\w\']+', currentLine)
+
+        for word in words:
+            # making all words case insensitive
+            lowWord = word.lower()
+            # making words in list lowercase & replacing their former case sensitive form
+            ind = words.index(word)
+            words[ind] = lowWord
+            
+        tokenList += words          # update list of tokens
+
+        currentLine = f.readline()  # moved onto next line
+
+    f.close()   # closes textfile
 
     return tokenList
 
