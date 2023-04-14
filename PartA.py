02f55c29d36da1f713d5450c7e9f6e713cca855f
Jessica <allenje1@uci.edu>
2023-04-13 22:44:54 -0700

    PartA format fixes

--- PartA.py
+++ PartA.py
@@ -1,62 +1,59 @@
-import re
+from regex import re
 
-# Tokenize function #
-def tokenize(textFilePath):
+# tokenize function #
+def tokenize(textfilepath):
     # stores tokens to be returned later
-    tokenReturn = []
+    returnlist = []
 
-    # open & read files
-    f = open(textFilePath, 'r')
+    # open & read file line by line
+    f = open(textfilepath, 'r')
 
-    # read file line-by-line
-    currentLine = f.readline()
-    while currentLine:
+    # reading file line by line
+    currentline = f.readline()
+    while currentline:
         # find all words (including words with an apostrophe)
         # credit for regex guide: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
-        words = re.findall(r"[\w\']+", currentLine)
+        words = re.findall(r"[\w\']+", currentline)
 
         for word in words:
             # making all words lowercase
-            lowWord = word.lower()
-            # making words in list lowercase & replacing their former case sensitive forms
-            ind = word.index(word)
-            words[ind] = lowWord
+            lowword = word.lower()
+            # making words in list lowercase & replacing their previous case sensitive form
+            ind = words.index(word)
+            words[ind] = lowword
 
-        tokenReturn += words    # update list of tokens
-        currentLine = f.readline()  # move onto next line
+        returnlist += words     # update list of tokens
+        currentline = f.readline()  # move onto next line
 
-    f.close()   # close text file
+    f.close     # close file
 
-    return tokenReturn
+    return returnlist
 
+# computeWord function #
+def computeWordFrequencies(tokenlist):
+    # will store key-value pairs of tokens
+    returndict = {}
 
-
-# computeWordFrequencies #
-def computeWordFrequencies(TokenList):
-    # maps tokens and their count. Will be returned later.
-    tokenCountMap = {}
-
-    # entering/updating the frequency of each word in the dictionary
-    for word in TokenList:
-        inDict = word in tokenCountMap
-        
-        # if word is not in tokenCountMap, add it to the dictionary as the first instance of it
-        if inDict == False:
-            tokenCountMap[word] = 1
-        # if word exists in tokenCountMap, update the frequency of it
+    # entering/updating the frequency of words in dictionary
+    for word in tokenlist:
+        indict = word in returndict
+        # if word is not in returndict, add it to dictionary as first instance
+        if indict == False:
+            returndict[word] = 1
+        # if it is in the dictionary +1 to number of instances
         else:
-            freq = tokenCountMap.get(word)
+            freq = returndict.get(word)
             freq += 1
-            tokenCountMap[word] = freq
+            returndict[word] = freq
 
-    return tokenCountMap
+    return returndict
 
 
 # print #
-def printFrequencies(TokenCountMap) -> None:
+def printFrequencies(tokencountmap) -> None:
     # sort dictionary from most repeated to least repeated words
     # credit for sorted: https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/
-    sortMap = sorted(TokenCountMap.items(), key=lambda x:x[1], reverse=True)
+    sortMap = sorted(tokencountmap.items(), key=lambda x:x[1], reverse=True)
 
     # printing each key-value pair
     for pair in sortMap:
