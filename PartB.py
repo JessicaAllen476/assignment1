d9cdf33ff2a4efe9e1dbe11c370938169967eefc
Jessica <allenje1@uci.edu>
2023-04-13 22:45:26 -0700

    PartB format fixes

--- PartB.py
+++ PartB.py
@@ -1,10 +1,10 @@
 from PartA import tokenize
 
 # intersection #
-def intersection(TextFile1, TextFile2):
+def intersection(textfile1, textfile2):
     # tokenizing files
-    tfile1 = tokenize(TextFile1)
-    tfile2 = tokenize(TextFile2)
+    tfile1 = tokenize(textfile1)
+    tfile2 = tokenize(textfile2)
 
     # finding the intersection
     # credit: https://docs.python.org/2/library/sets.html
